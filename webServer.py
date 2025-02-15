cbffb9a1987b34e21c2a8b87122127613bcf2b68
TAPSOBA <it2284@nyu.edu>
2025-02-14 23:44:39 -0500

    commenting the comment 2

--- webServer.py
+++ webServer.py
@@ -19,15 +19,13 @@ def webServer(port=13331):
             htmlcontent = f.read()
             f.close()
 
-            outputdata = (
-                "HTTP/1.1 200 OK\r\n"
-                "Content-Type: text/html; charset=UTF-8\r\n"
-                "Server: MySimpleServer\r\n"
-                "Connection: close\r\n"
-                "\r\n"
-            )
+            outputdata += "HTTP/1.1 200 OK\r\n"
+            outputdata += "Content-Type: text/html; charset=UTF-8\r\n"
+            outputdata += "Server: MySimpleServer\r\n"
+            outputdata += "Connection: close\r\n"
+            outputdata += "\r\n"
+            outputdata += htmlcontent
             connectionSocket.sendall(outputdata.encode())
-            connectionSocket.sendall(htmlcontent)
 
             for i in f:
                 connectionSocket.sendall(i)
